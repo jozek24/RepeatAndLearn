27aea4891b6995d22fed8e8715d57ec56968b3d0
Józef Salik <jozek243@gmail.com>
2019-08-22 19:18:07 +0200

    add YandesxApi, Dictionary, MyViewModel classes

---
+++ RepeatAndLearn/RepeatAndLearn/Model/Dictionary.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepeatAndLearn.Model
+{
+    public class Dictionary
+    {
+        public int Code { get; set; }
+        public string Lang { get; set; }
+        public List<string> Text { get; set; }
+
+    }
+}
---
+++ RepeatAndLearn/RepeatAndLearn/Model/YandexApi.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RestSharp;
+using RestSharp.Authenticators;
+
+namespace RepeatAndLearn.Model
+{
+    public class YandexApi
+    {
+        const string BaseUrl = "https://translate.yandex.net/api/v1.5/tr.json/detect?";
+
+        readonly IRestClient _client;
+
+        string _accountSid;
+
+        public YandexApi(string accountSid, string secretKey)
+        {
+            _client = new RestClient(BaseUrl);
+            _client.Authenticator = new HttpBasicAuthenticator(accountSid, secretKey);
+            _accountSid = accountSid;
+        }
+
+        public T Execute<T>(RestRequest request) where T : new()
+        {
+            request.AddParameter("AccountSid", _accountSid, ParameterType.UrlSegment); // used on every request
+            var response = _client.Execute<T>(request);
+
+            if (response.ErrorException == null)
+                return response.Data;
+
+            const string message = "Error retrieving response.  Check inner details for more info.";
+            var twilioException = new ApplicationException(message, response.ErrorException);
+            throw twilioException;
+
+        }
+    }
+}
